2500cf5f466f3f3b9711caca968b892b4e821de1
xaldakr <xavieravilaposada@gmail.com>
2024-04-15 16:47:51 -0600

    Agregar archivos de proyecto.

---
+++ Dulce Sabor Modulo Administrador/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dulce_Sabor_Modulo_Administrador.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
