6d690bc4efa524a4d2fdab1605862175b4fbbbbb
xaldakr <xavieravilaposada@gmail.com>
2024-04-15 19:38:48 -0600

    Minimun changes XD

---
+++ Dulce Sabor Modulo Administrador/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dulce_Sabor_Modulo_Administrador.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
