14781cf9c1569aa87794e9eec5bf07347f44a433
xaldakr <xavieravilaposada@gmail.com>
2024-04-25 22:48:03 -0600

    a

--- Dulce Sabor Modulo Administrador/Controllers/AdminController.cs
+++ Dulce Sabor Modulo Administrador/Controllers/AdminController.cs
@@ -8,5 +8,40 @@ namespace Dulce_Sabor_Modulo_Administrador.Controllers
         {
             return View();
         }
+        //Tabla segun fecha
+        public IActionResult tablaFechas()
+        {
+            return View();
+        }
+        //Interno a tablaFechas
+        public IActionResult formularioFechas()
+        {
+            return RedirectToAction("tablaFechas");
+        }
+        //Index a tablaFechas
+        public IActionResult enviarFecha()
+        {
+            return RedirectToAction("tablaFechas");
+        }
+        //Tabla segun Mes
+        public IActionResult tablaMes()
+        {
+            return View();
+        }
+        //Interno a tablaMes
+        public IActionResult formularioMes()
+        {
+            return RedirectToAction("tablaFechas");
+        }
+        //Index a tablaMes
+        public IActionResult enviarMes()
+        {
+            return RedirectToAction("tablaFechas");
+        }
+        //regreso general
+        public IActionResult regresar()
+        {
+            return RedirectToAction("Index");
+        }
     }
 }
