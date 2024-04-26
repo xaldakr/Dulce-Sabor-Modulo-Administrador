ed7215ff52f34d8d127e913d155457ce90d944c3
xaldakr <xavieravilaposada@gmail.com>
2024-04-25 23:08:54 -0600

    a

--- Dulce Sabor Modulo Administrador/P2_Grupo_2_Meseros.sql
+++ Dulce Sabor Modulo Administrador/P2_Grupo_2_Meseros.sql
@@ -24,13 +24,6 @@ CREATE TABLE Mesa(
 	estado_mesa bit NULL,
 );
 
-CREATE TABLE DetallesPedidos(
-    id_detalle_pedido int IDENTITY(1,1) PRIMARY KEY,
-    id_venta int,
-    fecha_creacion date,
-    precio float,
-    FOREIGN KEY (id_venta) REFERENCES Ventas(id)
-);
 
 CREATE TABLE Ventas(
     id int IDENTITY(1,1) PRIMARY KEY,
